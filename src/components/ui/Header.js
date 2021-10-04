6a85a33d2b6c21856aabf62a52f7b2d2a81d922f
huynguyen <nguyendanghuy071195@gmail.com>
2021-10-04 17:56:29 +0700

    add material guide

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import { AppBar, Toolbar } from "@mui/material"
+
+const Header = props => {
+  return (
+    <AppBar>
+      <Toolbar>Arc Development</Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
